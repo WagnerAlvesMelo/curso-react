d992eec27c5a2a16c5f33368eb8828a2c109ea8c
WagnerAlvesMelo <walves13@outlook.com>
2019-12-18 23:00:39 -0300

    Componente autor adicionado

--- src/App.js
+++ src/App.js
@@ -1,53 +1,9 @@
 import React, {Component} from 'react';
 import './App.css';
-import InputCustomizado from './components/InputCustomizado';
+import AutorBox from './Autor'
+
 
 class App extends Component {
-	constructor(){
-		super();
-		this.state = {lista:[], nome:'', email:'', senha:''};
-		this.cadastraForm	= this.cadastraForm.bind(this);
-		this.setNome 		= this.setNome.bind(this);
-		this.setEmail 		= this.setEmail.bind(this);
-		this.setSenha		= this.setSenha.bind(this);
-	};
-	componentDidMount(){
-		fetch('http://cdc-react.herokuapp.com/api/autores')
-		.then(response => response.json())
-		.then((data)=>{
-			this.setState({lista:data})
-		})
-	}
-
-	cadastraForm(evento){
-		evento.preventDefault();
-		fetch('http://cdc-react.herokuapp.com/api/autores',{
-			method:'POST',
-			headers:{
-				"Content-Type" : "application/json"	
-			},
-			body: JSON.stringify({
-				nome:this.state.nome, email: this.state.email, senha: this.state.senha
-			})
-		}).then((response)=>{
-			response.json()
-			.then((resultado)=>{
-				this.setState({lista:resultado});
-			})
-		})
-	}
-
-	setNome(evento){
-		this.setState({nome:evento.target.value})
-	}
-
-	setEmail(evento){
-		this.setState({email:evento.target.value})
-	}
-
-	setSenha(evento){
-		this.setState({senha:evento.target.value})
-	}
 
 	render() {
 		return (
@@ -77,40 +33,7 @@ class App extends Component {
 						<h1>Cadastro de Autores</h1>
 					</div>
 					<div className="content" id="content">
-						<div className="pure-form pure-form-aligned">
-						<form className="pure-form pure-form-aligned" onSubmit={this.cadastraForm}>
-							<InputCustomizado id="nome" label="Nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome}/>
-							<InputCustomizado id="email" label="Email" type="email" name="email" value={this.state.email} onChange={this.setEmail}/>
-							<InputCustomizado id="senha" label="Senha" type="password" name="senha" value={this.state.senha} onChange={this.setSenha}/>
-							<div className="pure-control-group">
-								<label></label> 
-								<button type="submit" className="pure-button pure-button-primary">Gravar</button>                                    
-							</div>
-						</form>             
-		
-						</div>  
-						<div>            
-						<table className="pure-table">
-							<thead>
-							<tr>
-								<th>Nome</th>
-								<th>email</th>
-							</tr>
-							</thead>
-							<tbody>
-							{
-								this.state.lista.map(dados=>{
-									return (
-										<tr key={dados.id}>
-											<td>{dados.nome}</td>
-											<td>{dados.email}</td>
-										</tr>
-									);
-								})
-							}
-							</tbody>
-						</table> 
-						</div>             
+						<AutorBox/>
 					</div>
 				</div>            
 			</div>     
---
+++ src/Autor.js
@@ -0,0 +1,122 @@
+import React,{Component} from 'react';
+import InputCustomizado from './components/InputCustomizado';
+
+class FormularioAutor extends Component{
+    constructor(){
+        super();
+        this.state = {nome:'', email:'', senha:''}
+        this.cadastraForm	= this.cadastraForm.bind(this);
+		this.setNome 		= this.setNome.bind(this);
+		this.setEmail 		= this.setEmail.bind(this);
+		this.setSenha		= this.setSenha.bind(this);
+    }
+
+    cadastraForm(evento){
+		evento.preventDefault();
+		fetch('http://cdc-react.herokuapp.com/api/autores',{
+			method:'POST',
+			headers:{
+				"Content-Type" : "application/json"	
+			},
+			body: JSON.stringify({
+				nome:this.state.nome, email: this.state.email, senha: this.state.senha
+			})
+		}).then((response)=>{
+			response.json()
+			.then((resultado)=>{
+				this.props.atualizaListagem(resultado);
+			})
+		})
+	}
+
+	setNome(evento){
+		this.setState({nome:evento.target.value})
+	}
+
+	setEmail(evento){
+		this.setState({email:evento.target.value})
+	}
+
+	setSenha(evento){
+		this.setState({senha:evento.target.value})
+	}
+
+    render(){
+        return(
+        <div className="pure-form pure-form-aligned">
+        <form className="pure-form pure-form-aligned" onSubmit={this.cadastraForm}>
+            <InputCustomizado id="nome" label="Nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome}/>
+            <InputCustomizado id="email" label="Email" type="email" name="email" value={this.state.email} onChange={this.setEmail}/>
+            <InputCustomizado id="senha" label="Senha" type="password" name="senha" value={this.state.senha} onChange={this.setSenha}/>
+            <div className="pure-control-group">
+                <label></label> 
+                <button type="submit" className="pure-button pure-button-primary">Gravar</button>                                    
+            </div>
+        </form>             
+
+        </div>  );
+    }
+}
+
+class TabelaAutores extends Component{
+    constructor(){
+        super();
+    }
+    render(){
+        return(
+            <div>            
+                <table className="pure-table">
+                    <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>email</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {
+                        this.props.lista.map(dados=>{
+                            return (
+                                <tr key={dados.id}>
+                                    <td>{dados.nome}</td>
+                                    <td>{dados.email}</td>
+                                </tr>
+                            );
+                        })
+                    }
+                    </tbody>
+                </table> 
+            </div>   
+        )
+    }
+}
+
+export default class AutorBox extends Component{
+
+    constructor() {
+        super();
+        this.state = {lista : []};
+        this.atualizaListagem = this.atualizaListagem.bind(this);
+    }
+	componentDidMount(){
+		fetch('http://cdc-react.herokuapp.com/api/autores')
+		.then(response => response.json())
+		.then((resposta)=>{
+			this.setState({lista:resposta})
+        })
+    }
+    
+
+    atualizaListagem(retorno){
+        this.setState({lista:retorno});
+    }
+
+    render(){
+        console.log(this.state.lista);
+        return(
+            <div>
+                <FormularioAutor atualizaListagem = {this.atualizaListagem}/>
+                <TabelaAutores lista={this.state.lista}/>
+            </div>
+        );
+    }
+}
